53fc639efabdeadafe939e1579d636e5f32ff604
aguroshou <aguroshou0413@gmail.com>
2021-11-26 19:56:09 +0900

    add: 的に弾を当てたら次のランダムな的が表示される状態です。

--- RadiconShotARUnityProject/Assets/Aguro/BulletManager.cs
+++ RadiconShotARUnityProject/Assets/Aguro/BulletManager.cs
@@ -7,10 +7,12 @@ public class BulletManager : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
     [SerializeField] private int moveCountLimit;
+    [SerializeField] private bool isHit;
 
     // Start is called before the first frame update
     void Start()
     {
+        isHit = false;
         StartCoroutine("FireBullet");
     }
 
@@ -33,9 +35,30 @@ public class BulletManager : MonoBehaviour
 
     private void OnCollisionEnter(Collision collideObject)
     {
-        Debug.Log("Collide");
-        if (collideObject.gameObject.CompareTag("Target"))
+        if (isHit == false && this.gameObject.CompareTag("Target"))
         {
+            // 2回以上処理を実行させないためです。
+            isHit = true;
+            // どれだけ的の中心に当てたかスコア計算をします。
+            this.gameObject.transform.parent = collideObject.transform;
+            //this.gameObject.transform.rotation = collideObject.transform.rotation;
+            this.gameObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            Vector2 gameObjectXZ =
+                new Vector2(this.gameObject.transform.localPosition.x, this.gameObject.transform.localPosition.z);
+
+            float distance =
+                Vector2.Distance(new Vector2(0, 0), gameObjectXZ);
+            int addScore = 100 - (int)(distance * 100);
+            Debug.Log(addScore);
+            PlayerPrefs.SetInt("Score", PlayerPrefs.GetInt("Score") + addScore);
+
+            collideObject.gameObject.SetActive(false);
+
+            // 次の番号のターゲットをActiveにする
+            GameObject mainGameObject = GameObject.Find("MainGameScript");
+            MainGame mainGame = mainGameObject.GetComponent<MainGame>();
+            mainGame.SetNextTarget();
+
             Destroy(this.gameObject);
         }
     }
--- RadiconShotARUnityProject/Assets/Aguro/MainGame.cs
+++ RadiconShotARUnityProject/Assets/Aguro/MainGame.cs
@@ -3,23 +3,37 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Vuforia;
 
 public class MainGame : MonoBehaviour
 {
     [SerializeField] private GameObject arCamera;
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private List<GameObject> targetList;
+    [SerializeField] private Image nextTargetImage;
+    [SerializeField] private List<Image> nextTargetImageList;
 
     [SerializeField] private TextMeshProUGUI scoreText;
     [SerializeField] private TextMeshProUGUI timeText;
     [SerializeField] private int limitTime;
     [SerializeField] private int currentTime;
+    [SerializeField] private int currentTargetNumber;
 
     // Start is called before the first frame update
     void Start()
     {
+        for (int targetNumber = 0; targetNumber < 10; targetNumber++)
+        {
+            targetList[targetNumber].SetActive(false);
+        }
+
+        currentTargetNumber = Random.Range(0, 10);
+        targetList[currentTargetNumber].SetActive(true);
+        //nextTargetImage = nextTargetImageList[targetRandomNumber];
+        Debug.Log("Target = " + currentTargetNumber);
         currentTime = limitTime;
         StartCoroutine("CountDownTime");
+        PlayerPrefs.SetInt("Score", 0);
     }
 
     // Update is called once per frame
@@ -29,21 +43,37 @@ public class MainGame : MonoBehaviour
         {
             GameObject bullet = Instantiate(bulletPrefab, arCamera.transform.position, arCamera.transform.rotation);
 
-            // arCameraの子オブジェクトにするとカメラの中心位置に固定されますが、直進しているわけではないです。
+            // arCameraの子オブジェクトにするとカメラの中心位置に固定されますが、銃の弾が直進しなくなります。
             //GameObject bullet = Instantiate(bulletPrefab, arCamera.transform);
         }
 
-        scoreText.text = PlayerPrefs.GetInt("Score").ToString();
+        scoreText.text = "スコア：" + PlayerPrefs.GetInt("Score").ToString();
+    }
+
+    public void SetNextTarget()
+    {
+        int nextTargetNumber;
+        do
+        {
+            nextTargetNumber = Random.Range(0, 10);
+        } while (nextTargetNumber != currentTargetNumber);
+
+        currentTargetNumber = nextTargetNumber;
+        targetList[currentTargetNumber].SetActive(true);
+        //nextTargetImage = nextTargetImageList[targetRandomNumber];
+        Debug.Log("Target = " + currentTargetNumber);
     }
 
     IEnumerator CountDownTime()
     {
-        while (currentTime>=0)
+        while (currentTime >= 0)
         {
             currentTime--;
             timeText.text = currentTime.ToString();
             yield return new WaitForSeconds(1.0f);
         }
+
         SceneManager.LoadScene("Result");
     }
+
 }
